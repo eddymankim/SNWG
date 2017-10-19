35c1eb89952d7a97dc4782c5073e8a854848bdf7
camibaumann <cbaumann@andrew.cmu.edu>
2017-10-19 14:25:48 -0400

    my new changes

--- _projects/cbaumann-6.md
+++ _projects/cbaumann-6.md
@@ -8,3 +8,23 @@ week-assignment: 6
 code: cbaumann/cbaumann-6
 ---
 
+<div class="grid-container" >
+  <div class="large-12 columns" >
+
+      <div class="row" style = "padding: 1em;">
+        <div class = "large-4 medium-4 colums">
+        </div>
+        <div class = "large-4 medium-4 colums">
+        </div>
+        <div class = "large-4 medium-4 colums">
+        </div>
+      </div>
+
+      <div class = "row" id="3dDiv" align = "center" style = "width:400, hieght:400">
+      </div>
+      
+</div> <!-- end large colummn -->
+</div><!-- end grid container-->
+
+
+<script src="/../code/cbaumann/cbaumann-6.js"></script>
--- code/cbaumann/cbaumann-6.js
+++ code/cbaumann/cbaumann-6.js
@@ -1,88 +1,90 @@
 
 import * as THREE from '../lib/module.js'
 
-//start scene
+//create scene
 var scene = new THREE.Scene();
-var aspect = window.innerWidth / window.innerHeight;
-scene.background = new THREE.Color(0xFAF8D7);
-scene.fog = new THREE.FogExp2(0xACC6AA, 0.002);
+var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+scene.background = new THREE.Color(0xC7E78B);
+scene.fog = new THREE.FogExp2(0xcccccc, 0.002);
+
+//create raycaster
+var raycaster = new THREE.Raycaster();
+
 
 //mouse
 var mouse = new THREE.Vector2(), INTERSECTED;
 var radius = 100, theta = 0;
 
-var container = document.createElement('div');
-document.body.appendChild(conteiner);
-
-var info = document.createElement('div');
-
-
-
-//set up camera
-var camera = new THREE.PerspectiveCamera(40, window.innerWidth/window.innerHeight, 0.1, 1000);
- camera.position.z = 500;
- camera.position.y = 50;
-
-
-//set up the renderer
+//create renderer
 var renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.getElementById('RenderCanvas').appendChild(renderer.domElement) ;
 
-var controls = new THREE.OrbitControls(camera, renderer.domElement);
-controls.addEventListener('change', render); 
-controls.enableZoom = false;
-
-//add the lights
-var ambientLight = new THREE.AmbientLight(0xFAF8D7);
+//create a light
+var ambientLight = new THREE.AmbientLight(0x143A52);
 scene.add(ambientLight);
 
 
-//make a cube
-var geometry = new THREE.BoxGeometry( 50, 50, 50 );
-var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+//Create a SpotLight and turn on shadows for the light
+var light = new THREE.SpotLight( 0xffffff );
+light.castShadow = true;            // default false
+scene.add( light );
+//Set up shadow properties for the light
+light.shadow.mapSize.width = 512;  // default
+light.shadow.mapSize.height = 512; // default
+light.shadow.camera.near = 0.5;       // default
+light.shadow.camera.far = 500 ;     // default
+
+
+//create geometry
+var geometry = new THREE.BoxGeometry( 20, 20, 20);
+var material = new THREE.MeshBasicMaterial( { color: 0x6E828A } );
 var cube = new THREE.Mesh( geometry, material );
 cube.castShadows = true;
 scene.add( cube );
 
+camera.position.z = 5;
 
+
+
+//mouse function
 function onDocumentMouseMove( event ) {
 				event.preventDefault();
 				mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
 				mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
 			}
 
-//rotates cube and calculates if mouse intersects with cube
-var render = function() {
+function animate() {
+	requestAnimationFrame( animate );
+    cube.rotation.x += 0.001;
+    cube.rotation.y += 0.001;
+    
+	renderer.render( scene, camera );
+}
+animate();
+
+function render(){
     
     theta += 0.1;
-				camera.position.x = radius * Math.sin( THREE.Math.degToRad( theta ) );
-				camera.position.y = radius * Math.sin( THREE.Math.degToRad( theta ) );
-				camera.position.z = radius * Math.cos( THREE.Math.degToRad( theta ) );
-				camera.lookAt( scene.position );
-				camera.updateMatrixWorld();
-				// find intersections
-				raycaster.setFromCamera( mouse, camera );
-				var intersects = raycaster.intersectObjects( scene.children );
-				if ( intersects.length > 0 ) {
-					if ( INTERSECTED != intersects[ 0 ].object ) {
+    camera.position.x = radius * Math.sin( THREE.Math.degToRad( theta ) );
+    camera.position.y = radius * Math.sin( THREE.Math.degToRad( theta ) );
+    camera.position.z = radius * Math.cos( THREE.Math.degToRad( theta ) );
+    camera.lookAt( scene.position );
+    
+    // find intersections
+    raycaster.setFromCamera( mouse, camera );
+    var intersects = raycaster.intersectObjects( scene.children );
+    if ( intersects.length > 0 ) {
+        var targetDistance = intersects[ 0 ].distance;
+        if ( INTERSECTED != intersects[ 0 ].object ) {
 						if ( INTERSECTED ) INTERSECTED.material.emissive.setHex( INTERSECTED.currentHex );
 						INTERSECTED = intersects[ 0 ].object;
 						INTERSECTED.currentHex = INTERSECTED.material.emissive.getHex();
-						INTERSECTED.material.emissive.setHex( 0xff0000 );
+						INTERSECTED.material.emissive.setHex( 0xE3EFF3);
 					}
 				} else {
 					if ( INTERSECTED ) INTERSECTED.material.emissive.setHex( INTERSECTED.currentHex );
 					INTERSECTED = null;
 				}
-	
-	requestAnimationFrame(render);
-		cube.rotation.x += 0.001;
-		cube.rotation.y += 0.01;
-		cube.rotation.z += 0.001;
-	renderer.render(scene, camera);
-};
-
+}
 render();
-
-
