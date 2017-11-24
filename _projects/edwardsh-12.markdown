ed66cd9bed21b0a516e4e63708a58ac1f20d2a57
Edward Shin <edshin7@gmail.com>
2017-11-24 10:15:50 -0500

    Week 12 progress

---
+++ _projects/edwardsh-12.markdown
@@ -0,0 +1,38 @@
+---
+layout: code
+author: Edward Shin
+title: Shin - Final Project Phase 2
+thumbnail: shin_thumb_6.png
+week-assignment: 12 - 13
+code: edwardsh/week_12
+---
+
+<h2>Phase 2:</h2>
+
+<p>
+    For phase 2, the hope is that the player would be able to move and the bridge would continue to porperly be moved and updated for continued use. For a better experience, I also want the camera to be able to rotate to some degree without negatively affecting the rotation of the rotation nodes. This way, the player can enjoy the environment more freely than just staring stiffly in one direction. Lastly, I need to write code setting and resetting a goal object for player's to find and interact with to explore new worlds and experience over and over. This will mean getting some ideas on how and what to generate.
+    
+    In short, this phase is about finishing and polishing the technical parts and getting started with the aesthetics of the game.
+</p>
+
+<h2>Progress: </h2>
+
+<p>
+    Like with Phase 1, I did encounter a few errors and unintended results, but I did manage to finish a majority of the technical parts of the program. 
+    
+    What I am mostly concerned about is how much narrative is involved in this game. While I do believe the inclusion of the goal and restting of the world will give players an incentive to continue playing and exploring at different angles, I cannot help but feel that there must be something more I can do to bring out an even more enriching experience for the players. 
+    
+    My choice in the the types and methods of generating environemnts will certainly play an improtant role in building a strong narrative for the game. I'm also looking at games like Monument Valley, Antichamber, and Journey for ideas on how to expand from here.
+</p>
+
+<h2>What's Next: </h2>
+
+<p>
+    
+</p>
+
+<h2>Sketches: </h2>
+
+<img src="img/edwardsh/sketch1.jpg" alt="Sketch 1" height="400" width="600">
+
+<img src="img/edwardsh/sketch2.jpg" alt="Sketch 2" height="400" width="600">
---
+++ code/edwardsh/week_12_old1.js
@@ -0,0 +1,412 @@
+// Edward Shin
+// Project Axis
+// Phase 2
+// Start: 11-09-17
+// End: 
+
+import * as THREE from '../lib/module.js'
+
+var scene, skybox, camera, raycaster, renderer;    // three.js elements
+var nodes, bridgePieces, numBPieces, bridgeSize, bridgeSpace, gridLength, nodeSize, nodeSpace, end1, end2, activeEnd, otherEnd, nodeRange;    // parts for node and bridge
+var player, playerXAxis, playerYAxis, bridge, activeNode, otherSideNode;    // to be used for moving player and bridge
+var xIsRotating, yIsRotating, zIsRotating, cameraIsMoving;
+var rotInc, rotIncX, rotIncY, rotIncZ, rotXCount, rotYCount, rotZCount, stepInc, remainingRot, remainingSteps, xAxis, yAxis, zAxis;
+var cameraHeight;
+
+
+var Colors = {
+	red: 0xf25346,
+	white: 0xd8d0d1,
+	brown: 0x59332e,
+	pink: 0xF5986E,
+	brownDark: 0x23190f,
+	blue: 0x68c3c0
+};
+
+// create scene
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000);
+var raycaster = new THREE.Raycaster();
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.shadowMap.enabled = true;
+
+
+var skyLoader = new THREE.CubeTextureLoader();
+skyLoader.setPath( '../../code/evan-erdos/images/arrakis-day.hdr/' );
+skybox = new THREE.CubeTexture();
+scene.background = skyLoader.load( [
+	'nx.png', 'px.png',
+	'py.png', 'ny.png',
+	'pz.png', 'nz.png'
+] );
+
+
+
+// lights
+var hemisphereLight = new THREE.HemisphereLight(0xaaaaaa, 0x000000, .9);
+var ambientLight = new THREE.AmbientLight(0xdc8874, 0.5);
+var shadowLight = new THREE.DirectionalLight(0xffffff, .7);
+
+shadowLight.position.set(10, 100, 10);
+
+shadowLight.castShadow = true;
+scene.add(hemisphereLight);
+scene.add(ambientLight);
+scene.add(shadowLight);
+
+
+nodes = [];   // list of rotation nodes;
+bridgePieces = [];    // list of bridge pieces
+numBPieces = 5;       // number of bridge pieces
+bridgeSize = 1;       
+bridgeSpace = 0.125;    // spacing between bridge pieces
+gridLength = 5;         
+nodeSize = 1;
+nodeSpace = ((numBPieces + 1) * bridgeSize) + ((numBPieces + 2) * bridgeSpace);     // spacing between nodes
+nodeRange = 0.1;
+
+player = new THREE.Object3D();      // responsible to rotations and movements and ref for player z axis
+playerXAxis = new THREE.Object3D(); // reference for player x axis
+playerYAxis = new THREE.Object3D(); // reference for player y aixs
+playerXAxis.rotation.y = Math.PI / 2;
+playerYAxis.rotation.x = Math.PI / 2;
+bridge = new THREE.Object3D();  // bridge Object by combining bridge and end pieces
+
+xIsRotating = false;
+yIsRotating = false;
+zIsRotating = false;
+cameraIsMoving = false;
+
+rotInc = rotIncX = rotIncY = rotIncZ = Math.PI / 1000;
+rotXCount = rotYCount = rotZCount = 0;
+stepInc = 0.01;
+remainingRot = Math.PI / 2;
+remainingSteps = nodeSpace + bridgeSize + bridgeSpace;
+xAxis = playerXAxis.getWorldDirection().normalize();
+yAxis = playerYAxis.getWorldDirection().normalize();
+zAxis = player.getWorldDirection().normalize();
+
+cameraHeight = 0.85;
+
+
+
+
+
+
+
+
+
+function setScene() {
+    makeNodeGrid();
+    makeBridge();
+    scene.add(player);
+    scene.add(playerXAxis);
+    scene.add(playerYAxis);
+    player.add(playerXAxis);
+    player.add(playerYAxis);
+    camera.position.y = cameraHeight;
+}
+
+function makeNodeGrid() {
+    var start = -(gridLength - 1) / 2;
+    
+    for (var i = start; i < start + gridLength; i++) {
+        var x = i * nodeSpace;
+        
+        for (var j = start; j < start + gridLength; j++) {
+            var y = j * nodeSpace;
+            
+            for (var k = start; k < start + gridLength; k++) {
+                var z = k * nodeSpace;
+                var newNode = new Node();
+                
+                newNode.position.set(x, y, z);
+                nodes.push(newNode);
+            }
+        }
+    }
+    
+    // add nodes to the scene
+    for (var i = 0; i < nodes.length; i++) {
+        scene.add(nodes[i]);
+    }
+}
+
+function makeBridge() {
+    for (var i = 0; i < numBPieces; i++) {
+        var z = -(i + 1) * (bridgeSize + bridgeSpace);
+        var newBPiece = new BridgePiece();
+        
+        newBPiece.position.z = z;
+        bridge.add(newBPiece);
+    }
+    
+    end1 = new EndPiece();
+    end2 = new EndPiece();
+    end2.position.z = -(numBPieces + 1) * (bridgeSize + bridgeSpace);
+    bridge.add(end1);
+    bridge.add(end2);
+    
+    scene.add(bridge);
+    
+}
+
+function rotateCamera() {
+    if(xIsRotating) {
+        var q = new THREE.Quaternion();
+        q.setFromAxisAngle(xAxis.normalize(), rotIncX);
+        player.applyQuaternion(q);
+        remainingRot -= rotInc;
+        
+        if(remainingRot <= 0) {
+            remainingRot = Math.PI / 2;
+            resetOtherNode();
+            
+            player.updateMatrixWorld();
+            THREE.SceneUtils.detach(bridge, player, scene);
+            THREE.SceneUtils.detach(camera, player, scene);
+            THREE.SceneUtils.detach(activeNode, player, scene);
+            
+            updateAxis();
+            resetOtherNode();
+            xIsRotating = false;
+        }
+    }
+    
+    else if(yIsRotating) {
+        var q = new THREE.Quaternion();
+        q.setFromAxisAngle(yAxis.normalize(), rotIncY);
+        player.applyQuaternion(q);
+        remainingRot -= rotInc;
+        
+        if(remainingRot <= 0) {
+            remainingRot = Math.PI / 2;
+            resetOtherNode();
+            
+            player.updateMatrixWorld();
+            THREE.SceneUtils.detach(bridge, player, scene);
+            THREE.SceneUtils.detach(camera, player, scene);
+            THREE.SceneUtils.detach(activeNode, player, scene);
+            
+            updateAxis();
+            resetOtherNode();
+            yIsRotating = false;
+        }
+    }
+    
+    else if(zIsRotating) {
+        var q = new THREE.Quaternion();
+        q.setFromAxisAngle(zAxis.normalize(), rotIncZ);
+        player.applyQuaternion(q);
+        remainingRot -= rotInc;
+        
+        if(remainingRot <= 0) {
+            remainingRot = Math.PI / 2;
+            player.updateMatrixWorld();
+            THREE.SceneUtils.detach(bridge, player, scene);
+            THREE.SceneUtils.detach(camera, player, scene);
+            THREE.SceneUtils.detach(activeNode, player, scene);
+            
+            updateAxis();
+            resetOtherNode();
+            zIsRotating = false;
+        }
+    }
+}
+
+// update x, y, z axis to correspond with the rotated axis
+function updateAxis() {
+    xAxis = playerXAxis.getWorldDirection().normalize();
+    yAxis = playerYAxis.getWorldDirection().normalize();
+    zAxis = player.getWorldDirection().normalize();
+}
+
+// move camera to the node on the otherside of the bridge
+function moveCamera() {
+    if(cameraIsMoving) {
+        player.translateZ(stepInc);
+        remainingSteps -= Math.abs(stepInc);
+        
+        if(remainingSteps <= 0) {
+            player.position.set(otherSideNode.getWorldPosition().x, 
+                                otherSideNode.getWorldPosition().y, 
+                                otherSideNode.getWorldPosition().z);
+            
+            // switch active and other end nodes
+            var tempEnd = otherEnd;
+            otherEnd = activeEnd;
+            activeEnd = tempEnd;
+            
+            resetOtherNode();
+            
+//            var temp = end1;
+//            end1 = end2;
+//            end2 = temp;
+//            
+//            setActiveNode();
+            
+            player.updateMatrixWorld();
+            THREE.SceneUtils.detach(camera, player, scene);
+            
+            updateAxis();
+            
+            // rotate player Object back forward relative to the bridge
+            var q = new THREE.Quaternion();
+            q.setFromAxisAngle(yAxis.normalize(), Math.PI);
+            player.applyQuaternion(q);
+            
+            cameraIsMoving = false;
+        }
+    }
+}
+
+function setActiveNode() {
+    for(var i = 0; i < nodes.length; i++) {
+        var curNode = nodes[i];
+        var dx = Math.pow((player.getWorldPosition().x - curNode.getWorldPosition().x), 2);
+        var dy = Math.pow((player.getWorldPosition().y - curNode.getWorldPosition().y), 2);
+        var dz = Math.pow((player.getWorldPosition().z - curNode.getWorldPosition().z), 2);
+        var dist = Math.sqrt(dx + dy + dz);
+        
+        if(dist < nodeRange) {
+            activeNode = nodes[i];
+            activeEnd = end1;
+            otherEnd = end2;
+            break;
+        }
+    }
+}
+
+// FIX the MATH
+function resetOtherNode() {
+    for(var i = 0; i < nodes.length; i++) {
+        var curNode = nodes[i];
+        var dx = Math.pow((otherNode.getWorldPosition().x - curNode.getWorldPosition().x), 2);
+        var dy = Math.pow((otherNode.getWorldPosition().y - curNode.getWorldPosition().y), 2);
+        var dz = Math.pow((player.getWorldPosition().z - curNode.getWorldPosition().z), 2);
+        var dist = Math.sqrt(dx + dy + dz);
+        
+        if(dist < nodeRange) {
+            otherNode = nodes[i];
+            break;
+        }
+    }
+}
+
+// rotation nodes for player
+function Node() {
+    var nodeGeo = new THREE.BoxGeometry(nodeSize, nodeSize, nodeSize);
+    var nodeMat = new THREE.MeshLambertMaterial( { color: Colors.brown } );
+    var node = new THREE.Mesh(nodeGeo, nodeMat);
+
+    return node;
+}
+    
+// pieces of the moving bridge
+function BridgePiece() {
+    var pieceGeo = new THREE.BoxGeometry((nodeSize * 0.85), nodeSize, nodeSize);
+    var pieceMat = new THREE.MeshLambertMaterial( { color: Colors.white } );
+    var piece = new THREE.Mesh(pieceGeo, pieceMat);
+
+    return piece;
+}
+
+// end nodes of the moving bridge
+function EndPiece() {
+    var endSize = nodeSize * 0.1;
+    var pieceGeo = new THREE.BoxGeometry(endSize, endSize, endSize);
+    var pieceMat = new THREE.MeshLambertMaterial( { color: Colors.white, transparent: true, opacity: 0 } );
+    var piece = new THREE.Mesh(pieceGeo, pieceMat);
+    piece.rotation.y = Math.PI / 4;
+
+    return piece;
+}
+
+
+// controls for player to move around in the world
+function onKeyPress( event ) {
+    var k = String.fromCharCode(event.keyCode);
+    
+    if(k === 'x' || k === 'X') {
+        if(!xIsRotating && !yIsRotating && !zIsRotating && !cameraIsMoving) {
+            player.updateMatrixWorld();
+            THREE.SceneUtils.attach(camera, scene, player);
+            THREE.SceneUtils.attach(bridge, scene, player);
+            THREE.SceneUtils.attach(activeNode, scene, player);
+            
+            xIsRotating = true;
+        }
+    }
+    
+    else if(k === 'y' || k === 'Y') {
+        if(!xIsRotating && !yIsRotating && !zIsRotating && !cameraIsMoving) {
+            player.updateMatrixWorld();
+            THREE.SceneUtils.attach(camera, scene, player);
+            THREE.SceneUtils.attach(bridge, scene, player);
+            THREE.SceneUtils.attach(activeNode, scene, player);
+            
+            yIsRotating = true;
+        }
+    }
+    
+    else if(k === 'z' || k === 'Z') {
+        if(!xIsRotating && !yIsRotating && !zIsRotating && !cameraIsMoving) {
+            player.updateMatrixWorld();
+            THREE.SceneUtils.attach(camera, scene, player);
+            THREE.SceneUtils.attach(bridge, scene, player);
+            THREE.SceneUtils.attach(activeNode, scene, player);
+            
+            zIsRotating = true;
+        }
+    }
+    
+    else if(k === 'm' || k === 'M') {
+        if(!xIsRotating && !yIsRotating && !zIsRotating && !cameraIsMoving) {
+            player.updateMatrixWorld();
+            THREE.SceneUtils.attach(camera, scene, player);
+            remainingSteps = nodeSpace + bridgeSize + bridgeSpace;
+            
+            stepInc = -stepInc;
+            cameraIsMoving = true;
+        }
+    }
+}
+
+
+
+
+
+
+function loop() {	
+    rotateCamera();
+    moveCamera();
+    
+    renderer.render(scene, camera);
+    requestAnimationFrame( loop );
+}
+
+function init() {
+    setScene();
+    setActiveNode();
+    resetOtherNode();
+    
+    document.querySelector("#RenderCanvas").appendChild(renderer.domElement);
+    document.addEventListener("keypress", onKeyPress, false);
+    loop();
+}
+
+init();
+loop();
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
